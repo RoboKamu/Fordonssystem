d4a1d21a5090614d510d9704247237a541821cc3
RoboKamu <91156959+RoboKamu@users.noreply.github.com>
2023-06-13 22:41:50 +0200

    Create PrivatFordon.java

---
+++ PrivatFordon.java
@@ -0,0 +1,27 @@
+package slutuppgift.Klasser.privata;
+
+import java.util.Calendar;
+import slutuppgift.Klasser.Kund;
+import slutuppgift.Klasser.Fordon;
+
+/**
+ *
+ * @author karzan.murad
+ */
+public abstract class PrivatFordon extends Fordon{
+    
+    private String adress;
+    
+    public PrivatFordon(String regPlate, Kund owner, int maxPassengers, int wheels, Calendar date, float originalPrice, float currentPrice, float warranty, String adress) {
+        super(regPlate, owner, maxPassengers, wheels, date, originalPrice, currentPrice, warranty);
+        this.adress = adress;
+    }
+    
+    public void setAdress(String adressInput){
+        adress = adressInput;
+    }
+    
+    public String getAdress(String adressInput){
+        return adress;
+    }
+}
