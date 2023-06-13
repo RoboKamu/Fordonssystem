0025767734a7ed38d9547723412c457c0f1b8c6d
RoboKamu <91156959+RoboKamu@users.noreply.github.com>
2023-06-13 22:45:19 +0200

    Create KommunalFordon.java

---
+++ KommunalFordon.java
@@ -0,0 +1,29 @@
+package slutuppgift.Klasser.kommunala;
+
+import java.util.Calendar;
+import slutuppgift.Klasser.Fordon;
+import slutuppgift.Klasser.Kund;
+
+/**
+ *
+ * @author karzan.murad
+ */
+public abstract class KommunalFordon extends Fordon{
+
+    // skapa variable kommun då en kommunal fordon tillhör till en kommun inte specifik adress 
+    private String kommun;
+    
+    public KommunalFordon(String regPlate, Kund owner, int maxPassengers, int wheels, Calendar date, float originalPrice, float currentPrice, float warranty, String kommun) {
+        super(regPlate, owner, maxPassengers, wheels, date, originalPrice, currentPrice, warranty);
+        this.kommun = kommun;
+    }
+    
+    // Setter
+    public void setKommun(String kommunInput){
+        kommun = kommunInput;
+    }
+    // Getter
+    public String getKommun(){
+        return kommun;
+    }    
+}
